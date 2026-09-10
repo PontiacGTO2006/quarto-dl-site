5dadeb4ad3bde4531eb5aaf0c7aab18f67163078
PontiacGTO2006 <101056722+PontiacGTO2006@users.noreply.github.com>
2026-09-10 16:48:58 -0500

    Finished (for now) work on the custom Ashryver classifier UI, adding confidence list, and prepared custom UI for the CIFAR model

---
+++ projects/cifar-controller.js
@@ -0,0 +1,4 @@
+/**
+ * Contains JavaScript fetching logic to hook up predictions from the CIFAR classifier's HuggingFace space to this Quarto site.
+ * Also contains the functionality for outputting brief information about the image upon prediction. Code still in progress.
+ */
